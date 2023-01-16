7ded70473b04f0f9119c17c841f7b7c639b77952
Jonas <jonas.sterr@gmx.net>
2023-01-16 14:46:14 +0100

    added directories to be removed before install

--- vmware-horizon-deck-installer.sh
+++ vmware-horizon-deck-installer.sh
@@ -5,7 +5,10 @@ wget https://download3.vmware.com/software/CART23FQ3_LIN64_2209/VMware-Horizon-C
 chmod +x VMware-Horizon-Client-2209-8.7.0-20616018.x64.bundle
 sudo steamos-readonly disable
 sudo rm -rf /etc/vmware*
+sudo rm -rf $HOME/.vmware/*
+sudo rmdir $HOME/.vmware
 sudo ./VMware-Horizon-Client-2209-8.7.0-20616018.x64.bundle --console --required
 THEME=`kreadconfig5 --key LookAndFeelPackage`
 sudo sed -i "s/Exec=vmware-view %u/Exec=env GTK_THEME=$THEME vmware-view %u/g" /usr/share/applications/vmware-view.desktop
 sudo steamos-readonly enable
+echo -e "this is\e[92m made by deckplosion.de"
